bf038f5dd35d84090f4d7f0bab2b0b3498cc1671
main <refabatya@gmail.com>
2023-07-07 16:24:36 +0500

    ke

--- index.js
+++ index.js
@@ -220,7 +220,7 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '6031609132:AAHAGpnJSMJp2YqxjhpGM1-jBuZIwX7MWZ8';
+const token = '6335857018:AAF8DcwkVgcczSwbAVaNgWxanaPMZffcY3I';
 
 const bot = new TelegramBot(token, { polling: true });
 
